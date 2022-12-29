169e4d1704a5f77eb9b6eaf8d8ef35e021a4d30f
Ramm759 <Prozac759>
2022-12-29 22:11:51 +0100

    Initial commit

---
+++ src/test/java/com/myjava/gestiondestock/GestiondestockApplicationTests.java
@@ -0,0 +1,11 @@
+package com.myjava.gestiondestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiondestockApplicationTests {
+
+
+
+}
